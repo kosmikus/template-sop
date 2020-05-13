39f1ce001c661a6ddbe2e5368ddf524ef38dbfa1
Andres Loeh <andres@well-typed.com>
2020-05-13 17:54:17 +0200

    More things work.

--- experiment/Instantiations.hs
+++ experiment/Instantiations.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE TemplateHaskell #-}
-module Instantiations (fromRow_Person) where
+module Instantiations where
 
 import Data.Functor.Identity
 import SOP
@@ -13,24 +13,27 @@ sappend_Foo foo1 foo2 = $$(sgsappend [|| foo1 ||] [|| foo2 ||])
 sappend_Foo' :: Foo -> Foo -> Foo
 sappend_Foo' = $$(sgsappend')
 
--- sappend_Pair :: (CodeC (LiftT a), CodeC (LiftT b), Semigroup a, Semigroup b) => Pair a b -> Pair a b -> Pair a b
--- sappend_Pair p1 p2 = $$(sgsappend [|| p1 ||] [|| p2 ||])
+sappend_Pair :: (Semigroup a, Semigroup b) => Pair a b -> Pair a b -> Pair a b
+sappend_Pair p1 p2 = $$(sgsappend [|| p1 ||] [|| p2 ||])
 
--- sappend_Pair' :: (Semigroup a, Semigroup b) => Pair a b -> Pair a b -> Pair a b
--- sappend_Pair' = $$(sgsappend')
+sappend_Pair' :: (Semigroup a, Semigroup b) => Pair a b -> Pair a b -> Pair a b
+sappend_Pair' = $$(sgsappend')
 
--- instance NFData a => NFData (Tree a) where
---   rnf t = $$(sgrnf [|| t ||])
+instance NFData a => NFData (Tree a) where
+  rnf t = $$(sgrnf [|| t ||])
 
 eq_Ordering :: Ordering -> Ordering -> Bool
 eq_Ordering o1 o2 = $$(sgeq [|| o1 ||] [|| o2 ||])
+
+instance Eq a => Eq (Tree a) where
+  (==) t1 t2 = $$(sgeq [|| t1 ||] [|| t2 ||])
 
 showEnum_Ordering :: Ordering -> String
 showEnum_Ordering o =
   $$(sgShowEnum (K "<" :* K "=" :* K ">=" :* Nil) [|| o ||])
 
-fromRow_Person :: RowParser Person
-fromRow_Person = $$(sgfromRow)
+-- fromRow_Person :: RowParser Person
+-- fromRow_Person = $$(sgfromRow)
 
--- test :: [Int] -> [Int] -> Bool
--- test = $$eqList
+test :: Eq a => [a] -> [a] -> Bool
+test = $$eqList
